a83fd34ac5388a0f076c1f92b37bda771b5be113
DDona <doanptand@gmail.com>
2020-08-30 11:15:59 +0700

    use viewmodel

--- app/src/main/java/com/ddona/mvvm/db/PokemonDatabase.java
+++ app/src/main/java/com/ddona/mvvm/db/PokemonDatabase.java
@@ -6,6 +6,8 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.ddona.mvvm.model.Pokemon;
+
 @Database(entities = {Pokemon.class}, version = 1, exportSchema = false)
 public abstract class PokemonDatabase extends RoomDatabase {
 
--- app/src/main/java/com/ddona/mvvm/ui/MainActivity.java
+++ app/src/main/java/com/ddona/mvvm/ui/MainActivity.java
@@ -1,17 +1,34 @@
 package com.ddona.mvvm.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.ddona.mvvm.R;
 import com.ddona.mvvm.db.PokemonDatabase;
+import com.ddona.mvvm.model.Pokemon;
+import com.ddona.mvvm.viewmodel.PokemonViewModel;
+import com.ddona.mvvm.viewmodel.PokemonViewModelFactory;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private PokemonViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this, new PokemonViewModelFactory(this.getApplication())).get(PokemonViewModel.class);
+        viewModel.getPokemonList().observe(this, new Observer<ArrayList<Pokemon>>() {
+            @Override
+            public void onChanged(ArrayList<Pokemon> pokemons) {
+                Log.d("doanpt", "pokemon size:" + pokemons.size());
+            }
+        });
     }
 }
