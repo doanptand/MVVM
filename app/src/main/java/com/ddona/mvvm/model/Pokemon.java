94c2eda47f451e8fbbc0b72e61df5600184000e3
DDona <doanptand@gmail.com>
2020-08-30 10:09:28 +0700

    add models

---
+++ app/src/main/java/com/ddona/mvvm/model/Pokemon.java
@@ -0,0 +1,38 @@
+package com.ddona.mvvm.model;
+
+public class Pokemon {
+
+    private int id;
+    private String name;
+
+    private String url;
+
+    public Pokemon(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
