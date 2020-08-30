ec5224c8d8f6edcc9cb6e97966795cb12f44800a
DDona <doanptand@gmail.com>
2020-08-30 13:53:00 +0700

    add binding for adapter

--- app/src/main/java/com/ddona/mvvm/adapter/PokemonAdapter.java
+++ app/src/main/java/com/ddona/mvvm/adapter/PokemonAdapter.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.ddona.mvvm.R;
+import com.ddona.mvvm.databinding.ItemPokemonBinding;
 import com.ddona.mvvm.model.Pokemon;
 
 import java.util.List;
@@ -25,15 +26,15 @@ public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pokemon, parent, false);
-        return new ViewHolder(view);
+        ItemPokemonBinding binding = ItemPokemonBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.tvName.setText(mPokemons.get(position).getName());
-        Glide.with(holder.imgAvatar).load(mPokemons.get(position).getUrl())
-                .into(holder.imgAvatar);
+        holder.binding.tvName.setText(mPokemons.get(position).getName());
+        Glide.with(holder.binding.imgAvatar).load(mPokemons.get(position).getUrl())
+                .into(holder.binding.imgAvatar);
     }
 
     public Pokemon getPokemonAt(int position) {
@@ -46,13 +47,11 @@ public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.ViewHold
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imgAvatar;
-        TextView tvName;
+        ItemPokemonBinding binding;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            imgAvatar = itemView.findViewById(R.id.img_avatar);
-            tvName = itemView.findViewById(R.id.tv_name);
+        public ViewHolder(ItemPokemonBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }
